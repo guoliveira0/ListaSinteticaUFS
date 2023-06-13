b21a08c7d312a7d70517fba4afdf0eab75f3d397
Luiz Gustavo <luizgustavo160206@gmail.com>
2023-06-13 13:57:57 -0300

    estruturapronta

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Unidades federativas do Brasil</title>
 </head>
 
 <body>
-    <h1>Titulo</h1>
+    <h1>Lista Sintética das Unidades Federativas do Brasil</h1>
     <table>
         <thead>
             <tr>
@@ -18,7 +18,14 @@
                 <th>Sede de Governo</th>
                 <th>Área(km²)</th>
                 <th>População(2014)</th>
-                <th>Densidade</th>
+                <th>Densidade(2005)</th>
+                <th>PIB(2015)</th>
+                <th>(&#37;Total)(2015)</th>
+                <th>PIB per capita(R&#36;)(2015)</th>
+                <th>IDH(2010)</th>
+                <th>Alfabetização(2016)</th>
+                <th>Mortalidade Infantil(2016)</th>
+                <th>Expectativa de Vida(2016)</th>
             </tr>
         </thead>
         <tbody>
@@ -41,18 +48,27 @@
                     'pibPercapita' =>16953.46,
                     'idh' => 0.663,
                     'alfabetizacao' => 86.9,
-                    'mortalidadeinfantil' => 98 
+                    'mortalidadeinfantil' => 98,
+                    'expectativavida' => 73.9 . 'anos' 
+
                 ),
                 'AL' => array(
-                    'bandeira' => '<img width="50" src="https://upload.wikimedia.org/wikipedia/commons/8/88/Bandeira_de_Alagoas.svg" alt="Bandeira do Alagoas">',
-                    'unidadeFederativa' => 'Alagoas',
-                    'abreviacao' => 'AL',
-                    'sedeGoverno' => 'Maceió',
-                    'area' => 16_4122.2,
-                    'populacao' => 795145,
-                    'Densidade' => 4.30,
-                    'PIB' => 13622000
+                    'bandeira' => '<img width="50" src="https://upload.wikimedia.org/wikipedia/commons/4/4c/Bandeira_do_Acre.svg" alt="Bandeira do Acre">',
+                    'unidadeFederativa' => 'Acre',
+                    'abreviacao' => 'AC',
+                    'sedeGoverno' => 'Rio Branco',
+                    'area' => number_format(164122.2, 1, ',', '.'),
+                    'populacao' => 795145 . '%',
+                    'densidade' => 4.30,
+                    'pib' => 13622000,
+                    'total' => 0.2,
+                    'pibPercapita' =>16953.46,
+                    'idh' => 0.663,
+                    'alfabetizacao' => 86.9,
+                    'mortalidadeinfantil' => 98,
+                    'expectativavida' => 73.9 . 'anos' 
                 ),
+
 
 
             );
@@ -64,9 +80,16 @@
                 echo "<td>{$UFS[$chaves]['unidadeFederativa']}</td>";
                 echo "<td>{$UFS[$chaves]['abreviacao']}</td>";
                 echo "<td>{$UFS[$chaves]['sedeGoverno']}</td>";
-                echo "<td>{$UFS[$chaves]['area']}</td></tr>";
-                echo "<td>{$UFS[$chaves]['populacao']}</td></tr>";
-            
+                echo "<td>{$UFS[$chaves]['area']}</td>";
+                echo "<td>{$UFS[$chaves]['populacao']}</td>";
+                echo "<td>{$UFS[$chaves]['densidade']}</td>";
+                echo "<td>{$UFS[$chaves]['pib']}</td>";
+                echo "<td>{$UFS[$chaves]['total']}</td>";
+                echo "<td>{$UFS[$chaves]['pibPercapita']}</td>";
+                echo "<td>{$UFS[$chaves]['idh']}</td>";
+                echo "<td>{$UFS[$chaves]['alfabetizacao']}</td>";
+                echo "<td>{$UFS[$chaves]['mortalidadeinfantil']}</td>";
+                echo "<td>{$UFS[$chaves]['expectativavida']}</td></tr>";
             }
 
 
